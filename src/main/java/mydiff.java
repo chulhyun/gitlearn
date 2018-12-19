7d79416ece1e3a3106a4d1ec9b77bfab87e5400d
chulhyun <jinspa@gmail.com>
2018-12-19 16:20:01 +0100

    Initial commit

---
+++ src/main/java/mydiff.java
@@ -0,0 +1,8 @@
+public class mydiff {
+    public void diff() {
+        System.out.println("diff 1");
+        System.out.println("diff 2");
+        System.out.println("diff 3");
+        System.out.println("diff 4");
+    }
+}
